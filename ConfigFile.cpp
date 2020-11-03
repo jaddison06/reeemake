d2c2a81e26b74be012976f2a44117582ad07e606
James Addison <addison.j@etoncollege.org.uk>
2020-11-03 10:36:58 +0000

    ok now we're acc ready to start some work

--- ConfigFile.cpp
+++ ConfigFile.cpp
@@ -10,6 +10,52 @@ std::string readEntireFile(fs::path file)
     return buf.str();
 }
 
+std::vector<std::string> ConfigFileParser::splitString(std::string * string, char delimiter)
+{
+    std::vector<std::string> tokens;
+    std::string token;
+    std::stringstream stream;
+    while (std::getline(stream, token, delimiter)) { tokens.push_back(token); };
+    if (tokens.size() == 0) { tokens.push_back(*string); }
+
+    return tokens;
+}
+
+// at this point we have a map of allowedCommands,
+// and argument count has already been checked
+void ConfigFileParser::ParseCommand(command *cmd, std::unordered_map<std::string, int> *map, ConfigOptions *config)
+{
+
+    if (!map->count(cmd->command))
+        {
+            logger.error("Command "+cmd->command+" unrecognised");
+        }
+        for (auto allowedCommand : allowedCommands)
+        {
+            if (allowedCommand.name == cmd->command)
+            {
+                std::optional<int> argCount = allowedCommand.argCount;
+                if (argCount && cmd->options.size() != *argCount)
+                {
+                    logger.error("Command "+allowedCommand.name+" expected "+std::to_string(*argCount)+" args, got "+std::to_string(cmd->options.size()));
+                }
+            }
+        }
+        switch ( map->at(cmd->command) )
+        {
+            case 0:
+            {
+                // depend
+                
+            }
+
+            default:
+            {
+                logger.error(cmd->command+" has not been fully implemented yet, sorry :)");
+            }
+            
+        }
+}
 
 void ConfigFileParser::ParseConfigFile(fs::path file, ConfigOptions *config)
 {
@@ -26,53 +72,35 @@ void ConfigFileParser::ParseConfigFile(fs::path file, ConfigOptions *config)
             {
                 // is a command w/ options
                 thisCommand.command = line.substr(0, found);
-                thisCommand.options = line.substr(found, line.length() - found);
+                std::string options = line.substr(found, line.length() - found);
+                thisCommand.options = splitString(&options);
             } else
             {
                 thisCommand.command = line;
-                thisCommand.options = "";
+                thisCommand.options = {};
             }
             commands.push_back(thisCommand);
         }
     }
 
+
     // map allowedCommands so we can switch it
+    //
+    // we do this here so we only have to do it once
     //
     // this is vv ugly apparently there's a better solution with
     // lambdas or smth
     // TODO:
     // https://stackoverflow.com/questions/650162/why-the-switch-statement-cannot-be-applied-on-strings
     // https://stackoverflow.com/questions/16388510/evaluate-a-string-with-a-switch-in-c/
-
     std::unordered_map<std::string, int> map;
     for (int i=0; i<allowedCommands.size(); i++)
     {
-        map.insert({allowedCommands.at(i), i});
+        map.insert({allowedCommands.at(i).name, i});
     }
-
     for (auto cmd : commands)
     {
-        if (!map.count(cmd.command))
-        {
-            logger.error("Command "+cmd.command+" unrecognised");
-        }
-        switch ( map.at(cmd.command) )
-        {
-            case 0:
-            {
-                // depend
-                size_t space = cmd.options.find(" ");
-                std::string first = cmd.options.substr(0, space);
-                std::string second = cmd.options.substr(space, cmd.options.length() - space);
-                
-            }
-
-            default:
-            {
-                logger.error(cmd.command+" has not been fully implemented yet, sorry :)");
-            }
-            
-        }
+        ParseCommand(&cmd, &map, config);
     }
 }
 
--- ConfigFile.h
+++ ConfigFile.h
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <iostream>
 #include <unordered_map>
+#include <optional>
 #include "VectorThings.h"
 #include "Logging.h"
 
@@ -13,17 +14,25 @@ namespace fs = std::filesystem;
 
 std::string readEntireFile(fs::path);
 
-enum SourceFileListType {blocklist, passlist};
-
-struct SourceFileList
-{
-    // default
-    SourceFileListType listType { blocklist };
-
-    // fs::path?
-    std::vector<fs::path> files;
+// source file or group
+//
+// include - either include this or exclude it
+enum SourceType {file, group};
+struct Source {
+    std::string name;
+    bool include;
+    SourceType type;
 };
 
+// dependency tracking - either add or remove dependency
+struct Dependency
+{
+    fs::path source;
+    fs::path depend;
+    bool doesDepend;
+};
+
+// store different values for one option
 struct PlatformOptions
 {
     std::string win;
@@ -31,10 +40,11 @@ struct PlatformOptions
     std::string linux;
 };
 
+
 // initialize w/ defaults
 struct ConfigOptions
 {
-    // my way or the high way
+    // my way or the highway
     PlatformOptions compiler
     {
         // for minGW; cygwin would use C:\cygwin64\bin\g++.exe
@@ -43,40 +53,107 @@ struct ConfigOptions
         "g++",
         "g++",
     };
-    SourceFileList buildFiles;
-    std::vector<std::string> postBuildCommnands;
+    std::vector<Source> sources;
+    std::vector<Dependency> dependOptions;
+    bool manualDepends = false;
+    bool manualIncludes = false;
+    std::vector<std::string> postBuildCommands;
+    std::vector<std::string> defines;
+    std::string binName;
     bool isInstalling;
-    std::string installName;   
+    std::string installName;
+    std::vector<std::string> libraries;
+    int cxxStandard = 17;
 
 };
 
 struct command
 {
     std::string command;
-    std::string options;
+    std::vector<std::string> options;
 };
 
-const std::vector<std::string> allowedCommands
+struct commandTemplate
+{
+    std::string name;
+    
+    // if this is uninitialised, treat it like infinity
+    std::optional<int> argCount;
+};
+
+const std::vector<commandTemplate> allowedCommands
         {
-            "depend",
-            "no-depend",
-            "manual-depends",
-            "source-files",
-            "include",
-            "include-group",
-            "exclude",
-            "exclude-group",
-            "library",
-            "std",
-            "comp",
-            "bin-name",
-            "install",
-            "system",
-            "system-now",
-            "define",
-            "win",
-            "mac",
-            "linux"
+            {
+                "depend",
+                2
+            },
+            {
+                "no-depend",
+                2
+            },
+            {
+                "manual-depends",
+                1
+            },
+            {
+                "manual-includes",
+                1
+            },
+            {
+                "include",
+                1
+            },
+            {
+                "include-group",
+                1
+            },
+            {
+                "exclude",
+                1
+            },
+            {
+                "exclude-group",
+                1
+            },
+            {
+                "library",
+                1
+            },
+            {
+                "std",
+                1
+            },
+            {
+                "comp",
+                1
+            },
+            {
+                "bin-name",
+                1
+            },
+            {
+                "install",
+                1
+            },
+            {
+                "system"
+            },
+            {
+                "system-now"
+            },
+            {
+                "define",
+                2
+            },
+            {
+                "win"
+            },
+            {
+                "mac"
+            },
+            {
+                "linux"
+            }
 };
 
 
@@ -92,6 +169,10 @@ class ConfigFileParser
         bool systemCommandAvailable(std::string *);
         bool isCommand(std::string *);
         bool isWhitespace(std::string *);
+
+        void ParseCommand(command *, std::unordered_map<std::string, int> *, ConfigOptions *);
+
+        std::vector<std::string> splitString(std::string *, char = *" ");
 
         Logger logger{"Reeemake.ConfigFileParser"};
 };
